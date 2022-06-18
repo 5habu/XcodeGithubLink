815667fae2af170031bbcbccea4212bf9a150311
5habu <black.widow.of.la.porte.27@gmail.com>
2022-06-18 20:19:56 +0900

    Initial Commit

---
+++ XcodeGithubLink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeGithubLink
+//
+//  Created by 5habu on 2022/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeGithubLink/XcodeGithubLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGithubLinkApp.swift
+//  XcodeGithubLink
+//
+//  Created by 5habu on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGithubLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
